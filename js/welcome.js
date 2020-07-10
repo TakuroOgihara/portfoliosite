bec743bb78c2aafd697e1c0ef5ffbccca30e05d5
TakuroOgihara <66544382+TakuroOgihara@users.noreply.github.com>
2020-07-10 21:49:17 +0900

    change welcome.js
    マウスストーカー実行タイミングの調整とコメント修正

--- js/welcome.js
+++ js/welcome.js
@@ -8,7 +8,7 @@ $(function () {
         transform: "translate(" + e.clientX + "px, " + e.clientY + "px)", //カーソルの位置
       });
     });
-  }, 1300); //マウスストーカー表示後に実行する
+  }, 1200); //マウスストーカー用の円の表示後に実行する
 
   setTimeout(function () {
     $(window).on("click", function (e) {
@@ -19,7 +19,7 @@ $(function () {
         $("#stalker span").css({
           display: "none", // Welcome非表示
         });
-      }, 450); //縮小後に一瞬webcomeが表示されるのを避ける
+      }, 450); //welcome縮小後に実行
 
       $("#stalker")
         .delay(500) //welcomeが縮小し消えた後に実行する
@@ -33,6 +33,8 @@ $(function () {
           $(this).addClass("open");
         });
 
+      //円が広がって消えていくアニメーション実行中に
+      //カーソルを動かした場合の調整
       setTimeout(function () {
         $(window).on("mousemove", function (e) {
           $("#stalker").css({
@@ -43,6 +45,7 @@ $(function () {
         });
       }, 500); //welcomeが縮小し消えた後に実行する
 
+      //円が広がって消えていく
       $("#overall-color").fadeOut(1000);
     });
   }, 1500); //円の表示演出が終わるまでクリックイベント発生不可
